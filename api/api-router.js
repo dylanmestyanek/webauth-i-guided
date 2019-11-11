cc5583da62f2d3c79923f694bb0150d622cd3718
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-11 11:30:15 -0600

    Hashed password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -8,6 +9,19 @@ router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+
+  const originalPassword = credentials.password;
+  credentials.password = hash;
+
+  res.status(201).json({ 
+    password: originalPassword, 
+    hash: hash
+  })
 });
 
 module.exports = router;
